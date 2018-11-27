6794a64ecf1c8ddf0f5f1bed6434f7b06628c370
jouyouyun <jouyouwen717@gmail.com>
2018-11-27 14:47:53 +0800

    compress: improve gzip

--- compress/gzip/gzip.go
+++ compress/gzip/gzip.go
@@ -4,9 +4,11 @@ import (
 	"bytes"
 	"compress/gzip"
 	"flag"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 var (
@@ -41,28 +43,70 @@ func main() {
 }
 
 func gzipCompress(files []string) ([]byte, error) {
-	var buf = new(bytes.Buffer)
-	var writer = gzip.NewWriter(buf)
+	var buf bytes.Buffer
+	writer, err := gzip.NewWriterLevel(&buf, gzip.BestCompression)
+	if err != nil {
+		return nil, err
+	}
 	for _, f := range files {
 		contents, err := ioutil.ReadFile(f)
 		if err != nil {
 			writer.Close()
 			return nil, err
 		}
+		writer.Name = f
 		_, err = writer.Write(contents)
 		if err != nil {
 			writer.Close()
 			return nil, err
 		}
+		err = writer.Close()
+		if err != nil {
+			return nil, err
+		}
+		writer.Reset(&buf)
 	}
 
-	err := writer.Close()
-	if err != nil {
-		return nil, err
-	}
 	return buf.Bytes(), nil
 }
 
 func gzipDecompress(filename string) error {
+	log.Print("[Decompress] filename:", filename)
+	contents, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+	var buf = bytes.NewBuffer(contents)
+	reader, err := gzip.NewReader(buf)
+	if err != nil {
+		return err
+	}
+	defer reader.Close()
+	dir := "/tmp/decompress"
+	err = os.MkdirAll(dir, 0755)
+	if err != nil {
+		return err
+	}
+	for {
+		reader.Multistream(false)
+		log.Print("[Decompress] file:", reader.Name)
+		contents, err := ioutil.ReadAll(reader)
+		if err != nil {
+			log.Print("[Compress] Failed to read:", err)
+			return err
+		}
+		fpath := filepath.Join(dir, filepath.Base(reader.Name))
+		err = ioutil.WriteFile(fpath, contents, 0644)
+		if err != nil {
+			return err
+		}
+		err = reader.Reset(buf)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
