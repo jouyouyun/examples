3c2e7013fd7f71d954de9a7f98e269914b2868fb
jouyouyun <yanbowen@uniontech.com>
2021-11-20 21:46:11 +0800

    Add sd-bus

---
+++ dbus/sd-bus/common.h
@@ -0,0 +1,8 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#define BUS_DEST "info.jouyouyun.system.Test"
+#define BUS_PATH "/info/jouyouyun/system/Test"
+#define BUS_IFC "info.jouyouyun.system.Test"
+
+#endif
